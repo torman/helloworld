123f1c8a691dd198049bff177a09d07afe556905
torman <torman_cheng@yahoo.com>
2013-09-14 17:25:58 -0400

    try to configure out the file permission issue

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 Hello Everyone, Hello again after adding umask 022 in .bashrc
-I can create index.php !!! Hooray!!
+I can create index.php !!! Hooray!! 
+Try to configure out the file permission issue 
